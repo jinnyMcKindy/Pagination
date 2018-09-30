050dc2e7b75d9b3af3e06b81fefbe20a0de8f40d
Kuznetsova Yulia <u.kuznetsova@iml.ru>
2018-09-30 18:37:09 +0300

    changing extension

---
+++ src/components/DotComponent.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="c2c-dot" v-on:click="$emit('click-dots', value)" v-if="Max">...</div>
+</template>
+<script>
+  export default {
+    name : 'dot-component',
+    props : ["Max", "value"],
+  };
+</script>
+
